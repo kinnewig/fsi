892ff5450814cf502bad915686ac61e6a23cd9d3
Sebastian Kinnewig <sebastian@kinnewig.org>
2025-09-08 14:10:46 +0200

    Add XML based parameter reader.

---
+++ include/parameter_reader.h
@@ -0,0 +1,65 @@
+/* ---------------------------------------------------------------------
+ *
+ * Copyright (C) 2025 Sebastian Kinnewig
+ *
+ * The code is licensed under the GNU Lesser General Public License as 
+ * published by the Free Software Foundation in version 2.1 
+ * The full text of the license can be found in the file LICENSE.md
+ *
+ * ---------------------------------------------------------------------
+ * Contact:
+ *   Thomas Wick
+ *   Leibniz Universität Hannover (LUH)
+ *   Institut für Angewandte Mathematik (IfAM)
+ *
+ *   Sebastian Kinnewig
+ *   Leibniz Universität Hannover (LUH)
+ *   Institut für Angewandte Mathematik (IfAM)
+ *
+ * Questions?
+ *   E-Mail: kinnewig@ifam.uni-hannover.de
+ *
+ * Date: Sep, 2025
+ *
+ * ---------------------------------------------------------------------
+ */
+
+#ifndef PARAMETER_HANDLER_H 
+#define PARAMETER_HANDLER_H 
+
+// deal.II
+#include <deal.II/base/function.h>
+#include <deal.II/base/parameter_handler.h>
+#include <deal.II/numerics/data_out.h>
+
+// Teuchos
+#include <Teuchos_XMLParameterListHelpers.hpp>
+
+namespace FSI {
+
+  using namespace dealii;
+
+  class ParameterReader {
+    public:
+      ParameterReader(std::string xml_file);
+
+      int get_integer(const std::string &entry_subsection_path, 
+                      const std::string &entry_string ) const;
+
+      double get_double(const std::string &entry_subsection_path, 
+                        const std::string &entry_string ) const;
+
+      std::string get_string(const std::string &entry_subsection_path, 
+                             const std::string &entry_string ) const;
+
+      Teuchos::RCP<Teuchos::ParameterList>&
+      get_parameter_list();
+
+    private:
+      Teuchos::RCP<Teuchos::ParameterList> parameter_list;
+  };
+
+} // namespace FSI
+
+#endif // PARAMETER_HANDLER_H 
+
